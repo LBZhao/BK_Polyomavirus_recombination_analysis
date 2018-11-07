6504412a8043740401bf9161f1314849db3c93c1
Linbo Zhao <zhaolinbo@gmail.com>
2018-11-07 16:40:48 -0500

    Parse all sequence file
    This script can parse all files quickly.

--- Readsequence.py
+++ Readsequence.py
@@ -112,10 +112,55 @@ def ismatch(seq,sequencedic):
                     #location.append((lookback[index]+(index-item)*25))
                 #else: continue
 '''
-with gzip.open(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118033/118033_GGACTCCT-TATCCTCT_S1_L001_R1_001.fastq.gz', 'rt') as reads, open(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118033/collect1', "w") as collect:
-    for record in SeqIO.parse(reads,"fastq"):
-        if record.seq in pGEMDIKDIC:
-            pass
-        else:
 
-#            SeqIO.write(record,collect,"fastq")
+filelist=(
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118033/118033_GGACTCCT-TATCCTCT_S1_L001_R1_001.fastq.gz',r'/mnt/d/01_pGEM7-DIK.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118034/118034_TCGACTAG-TATCCTCT_S2_L001_R1_001.fastq.gz',r'/mnt/d/02_DIK_Mock.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118035/118035_CCTAGAGT-TATCCTCT_S3_L001_R1_001.fastq.gz',r'/mnt/d/03_DIK_01DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118036/118036_GCGTAAGA-TATCCTCT_S4_L001_R1_001.fastq.gz',r'/mnt/d/04_DIK_03DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118037/118037_TTATGCGA-TATCCTCT_S5_L001_R1_001.fastq.gz',r'/mnt/d/05_DIK_05DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118038/118038_TCGCCTTA-TATCCTCT_S6_L001_R1_001.fastq.gz',r'/mnt/d/06_DIK_07DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118039/118039_CGTCTAAT-GTAAGGAG_S7_L001_R1_001.fastq.gz',r'/mnt/d/07_DIK_10DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118040/118040_TCGACTAG-GTAAGGAG_S8_L001_R1_001.fastq.gz',r'/mnt/d/08_DIK_15DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118041/118041_CCTAGAGT-GTAAGGAG_S9_L001_R1_001.fastq.gz',r'/mnt/d/09_DIK_20DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118042/118042_GCGTAAGA-GTAAGGAG_S10_L001_R1_001.fastq.gz',r'/mnt/d/10_DIK_30DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118043/118043_TTATGCGA-GTAAGGAG_S11_L001_R1_001.fastq.gz',r'/mnt/d/11_DIK_40DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118044/118044_TCGCCTTA-GTAAGGAG_S12_L001_R1_001.fastq.gz',r'/mnt/d/12_DIK_60DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118045/118045_CGTCTAAT-ACTGCATA_S13_L001_R1_001.fastq.gz',r'/mnt/d/13_DIK_DIK_DUN_60DPI.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118046/118046_TCGACTAG-ACTGCATA_S14_L001_R1_001.fastq.gz',r'/mnt/d/14_D3_Mock.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118047/118047_CCTAGAGT-ACTGCATA_S15_L001_R1_001.fastq.gz',r'/mnt/d/15_D3_DIK.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118048/118048_GCGTAAGA-ACTGCATA_S16_L001_R1_001.fastq.gz',r'/mnt/d/16_D3_DIK_Mutant.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118049/118049_TTATGCGA-ACTGCATA_S17_L001_R1_001.fastq.gz',r'/mnt/d/17_D3_Dun.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118050/118050_TCGCCTTA-ACTGCATA_S18_L001_R1_001.fastq.gz',r'/mnt/d/18_D5_Mock.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118051/118051_CGTCTAAT-GCGTAAGA_S19_L001_R1_001.fastq.gz',r'/mnt/d/19_D5_DIK_Mutant.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118052/118052_TCGACTAG-GCGTAAGA_S20_L001_R1_001.fastq.gz',r'/mnt/d/20_D5_DIK.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118053/118053_CCTAGAGT-GCGTAAGA_S21_L001_R1_001.fastq.gz',r'/mnt/d/21_D5_Dunlop.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118054/118054_GCGTAAGA-GCGTAAGA_S22_L001_R1_001.fastq.gz',r'/mnt/d/22_D7_Mock.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118055/118055_TTATGCGA-GCGTAAGA_S23_L001_R1_001.fastq.gz',r'/mnt/d/23_D7_DIK.fastq'),
+(r'/mnt/d/Box Sync/Imperiale Lab Notebooks/Linbo Zhao/Projects/BKPyV recombination/20181011 Sequencing Results/Run_2500/imperiale/Sample_118056/118056_TCGCCTTA-GCGTAAGA_S24_L001_R1_001.fastq.gz',r'/mnt/d/24_D7_DIK_Mutant.fastq'))
+
+
+for (path,outputpath) in filelist:
+    totalcounter=0
+    dumpcounter=0
+    collectcounter=0
+    with gzip.open(path, 'rt') as reads, open(outputpath, "w+") as collect:
+            for record in SeqIO.parse(reads,"fastq"):
+                totalcounter+=1
+                if record.seq in DIKDIC:
+                    dumpcounter+=1
+                elif record.seq in DUNDIC:
+                    dumpcounter+=1
+                else:
+                    for j in range(10):
+                        if record.seq[j*25:(j*25+25)] in DIKDIC25:
+                            SeqIO.write(record,collect,"fastq")
+                            collectcounter+=1
+                            break
+                        elif record.seq[j*25:(j*25+25)] in DUNDIC25:
+                            SeqIO.write(record,collect,"fastq")
+                            collectcounter+=1
+                            break
+    print("%.3f%% dumped in the first step. %.3f%% collect in the final step. %.3f%% items do not match at all." %((dumpcounter*100/totalcounter),(collectcounter*100/totalcounter),((totalcounter-dumpcounter-collectcounter)*100/totalcounter)))
+    reads.close()
+    collect.close()
