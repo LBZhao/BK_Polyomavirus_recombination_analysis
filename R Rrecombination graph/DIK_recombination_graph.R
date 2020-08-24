051dff713877a68f07fdd5854b524aecc90a6bef
Linbo Zhao <zhaolinbo@gmail.com>
2020-08-23 23:10:33 -0400

    Update option

--- R Rrecombination graph/DIK_recombination_graph.R
+++ R Rrecombination graph/DIK_recombination_graph.R
@@ -10,9 +10,11 @@ library(stringr)
 
 args = commandArgs(trailingOnly=TRUE)
 png(filename = paste(str_sub(args[1],end=-5),".png"), width = 1200, height = 1100, units = "px")
-Connection <- read.csv(file=args[1], header=FALSE, sep=",")
 
-if (args[2] == "-N"){
+if (length(args)<2) {
+  stop("Please provide Connection file and option -N for NCCR or -T for total genome", call.=FALSE)
+Connection <- read.csv(file=args[1], header=FALSE, sep=",")
+} else if (args[2] == "-N"){
 d1=data.frame(from=c("origin", "origin", "origin", "origin", "origin"), to=c("O","P","Q", "R", "S"))
 d2=data.frame(from="O", to=paste("subgroup", seq(1,142), sep="_"))
 d3=data.frame(from="P", to=paste("subgroup", seq(143,210), sep="_"))
@@ -20,8 +22,7 @@ d4=data.frame(from="Q", to=paste("subgroup", seq(211,249), sep="_"))
 d5=data.frame(from="R", to=paste("subgroup", seq(250,312), sep="_"))
 d6=data.frame(from="S", to=paste("subgroup", seq(313,375), sep="_"))
 hierarchy=rbind(d1, d2, d3, d4, d5, d6)
-}
-if (args[2] == "-T"){
+} else if (args[2] == "-T"){
   d1=data.frame(from=c("origin","origin","origin"), to=c("NCCR", "LATE", "EARLY"))
   d2=data.frame(from="NCCR", to=paste("subgroup", seq(1,375), sep="_"))
   d3=data.frame(from="LATE", to=paste("subgroup", seq(376,2676), sep="_"))
@@ -49,8 +50,7 @@ if (args[2] == "-N"){
 p +  geom_conn_bundle(data = get_con(from = from, to = to), width=0.9, alpha=0.02, colour="dodgerblue4", tension=0.9) +
   geom_node_point(aes(filter = leaf, x = x*1.05, y=y*1.05, colour=group),   size=5) +
   scale_colour_manual(values= rep( brewer.pal(5,"Set1") , 30))
-}
-if (args[2] == "-T"){
+} else if (args[2] == "-T"){
 p +  geom_conn_bundle(data = get_con(from = from, to = to), width=0.9, alpha=0.02, colour="dodgerblue4", tension=0.4) +
   geom_node_point(aes(filter = leaf, x = x*1.05, y=y*1.05, colour=group),   size=5) +
   scale_colour_manual(values= rep( brewer.pal(3,"Set1") , 30))
